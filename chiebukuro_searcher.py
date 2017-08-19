1df35849185785559e752c11b617bbdce80500c8
yu-ya4 <yuya4.matsumura@gmail.com>
2017-08-19 23:09:05 +0900

    Add search_questions_by_category()

--- chiebukuro_searcher.py
+++ chiebukuro_searcher.py
@@ -60,3 +60,39 @@ class ChiebukuroSearcher:
             start += interval
 
         return result
+
+    def search_questions_by_category(self, category, size):
+        '''
+        search for questions and get json response
+
+        Args:
+            query: str
+                検索クエリ
+            size: int
+                取得する検索結果数
+            fields: list[str]
+                検索対象にするフィールドの名称からなるリスト
+        Returns:
+            list[dict[str, str or int]]
+            questionのlist
+            json形式
+        '''
+
+        start = 0
+        interval = 1000
+        result = []
+        while start < size:
+            if size < interval:
+                interval = size
+
+            json = {"from": start, "size": interval,"query":{"match":{"category": category}}}
+            res = self.es.search(index=self.index, doc_type='questions', body=json)
+            questions = res['hits']['hits']
+            # if there is no result, break the loop
+            if not questions:
+                break
+
+            result.extend(questions)
+            start += interval
+
+        return result
